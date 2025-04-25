bd0be4c5ff446e75f327e8b93ec455eefb27b8e3
Peter Switch <202387707+peter-switch@users.noreply.github.com>
2025-04-25 20:33:14 +0200

    Create inventario.py

---
+++ inventario.py
@@ -0,0 +1,4 @@
+# El objetivo es diseñar una aplicación capaz de realizar operaciones CRUD (Crear, Leer, Actualizar y Eliminar) sobre un conjunto de productos.
+# Cada producto tendrá atributos como nombre, cantidad, precio y categoría, y la aplicación deberá permitir a los usuarios gestionar esta información a través de un menú interactivo por consola.
+# Durante este reto, aplicarás conceptos como el diseño de clases orientadas a objetos, conexión a bases de datos con la biblioteca mysql-connector-python, validación de entradas y manejo de excepciones.
+# Además, se espera que implementes una interfaz de usuario sencilla y funcional que permita realizar las operaciones con fluidez y control de errores.
