8115261965b9530b4902b205802948cc2030189e
Peter Switch <202387707+peter-switch@users.noreply.github.com>
2025-04-26 18:49:09 +0000

    Funcionando v0.1

--- inventario.py
+++ inventario.py
@@ -15,10 +15,10 @@ class Conexion:
 
     def __init__(self):
         #Valores necesarios para conectar a la base de datos
-        self.host="rizos.pro"
+        self.host=""
         self.database="u762720325_pruebas_python"
         self.user="u762720325_admin"
-        self.password="IBMpython99@@@"
+        self.password=""
 
         #Estos 2 valores son para crear el pool de conexiones
         self.pool_name="pool_conexiones"
@@ -71,24 +71,164 @@ class Conexion:
             print(f"Error al obtener conexión del pool: {e}")
 
     
-    class Inventario:
-         
-        def __init__(self,conexion):
+class Inventario:
+        
+    def __init__(self,conexion):
+        self.conexion=conexion
+        
             
+    def insertar_producto(self,nombre,cantidad,precio,categoria):
+
+        try:
+
+            conn=self.conexion.obtener_conexion() #Obtenemos la conexión del pool
+
+            cursor=conn.cursor() #A partir de aquí ya podemos trabajar con el cursor y consultas sql
+            
+            valores=(nombre,cantidad,precio,categoria)
+
+            sql="INSERT INTO productos (nombre, cantidad, precio, categoria) VALUES (%s,%s,%s,%s)"
+
+            
+            
+            cursor.execute(sql,valores)
+            conn.commit() #Confirma los cambios
+            
+            print("\nProducto añadido correctamente")
+
+        except Error as e:
+            
+            print(f"Error al insertar producto {e}")
+            
+
+        finally:
+            #Una vez procesada la consulta en la bbdd cerramos cursor y devolvemos la conexión al pool.
+            cursor.close()
+            conn.close()
+
+    def ver_productos(self):
+        try:
+
+            conn=self.conexion.obtener_conexion()
+            cursor=conn.cursor()
+            sql="SELECT * FROM productos ORDER BY cantidad"
+            cursor.execute(sql)
+            resultado=cursor.fetchall()
+            for producto in resultado:
+                print(f"Producto: {producto[0]} - Precio: {producto[1]} - Cantidad: {producto[2]} - Categoria: {producto[3]}")
+
+
+        except Error as e:
+            print(f"Error al hacer SELECT: {e}")
+        
+        finally:
+            cursor.close()
+            conn.close()
+
+    def borrar_producto(self,nombre):
+
+        try:
+
+            conn=self.conexion.obtener_conexion()
+            cursor=conn.cursor()
+            sql="DELETE FROM productos WHERE nombre=%s"
+            valor=(nombre,)#Necesitamos que sea una tupla para poder añadirlo al cursor
+            cursor.execute(sql,valor)
+            conn.commit() #Confirma los cambios
+            print("Producto eliminado correctamente")
+
+
+        except Error as e:
+            print(f"Error al eliminar producto: {e}")
+        
+        finally:
+            cursor.close()
+            conn.close()
+
+    def actualizar_producto(self,nuevo_nombre,nuevo_precio,nueva_cantidad,nueva_categoria,nombre_actual):
+
+        try:
+
+            conn=self.conexion.obtener_conexion()
+            cursor=conn.cursor()
+            sql="UPDATE productos SET nombre = %s, precio = %s, cantidad = %s, categoria = %s WHERE nombre = %s"
+            valor=(nuevo_nombre,nuevo_precio,nueva_cantidad,nueva_categoria,nombre_actual)#Necesitamos que sea una tupla para poder añadirlo al cursor
+            cursor.execute(sql,valor)
+            conn.commit()  #guardar cambios
+            print("Producto actualizado")
+
+
+        except Error as e:
+            print(f"Error al actualizar producto: {e}")
+        
+        finally:
+            cursor.close()
+            conn.close()
+
+class Menu:
+    #Recibe en el constructor el objeto inventario para poder trabajar con él
+    def __init__(self,inventario):
+        self.inventario=inventario
+
+    def mostrar_menu(self):
+        
+        print("\n***Menú Invetario***")
+        while True:
+
+            print("""
+01. Ver listado de productos.
+02. Insertar nuevos productos.
+03. Actualizar un producto.
+04. Eliminar un producto.
+05. Salir
+            """)
+            opcion=int(input("Selecciona opción: "))
+
+            if opcion==1:
+                self.inventario.ver_productos()
+
+            elif opcion==2:
+                nombre=input("Nombre: ")
+                cantidad=int(input("Cantidad: "))
+                precio=float(input("Precio :"))
+                categoria=input("Categoría: ")
+                self.inventario.insertar_producto(nombre,cantidad,precio,categoria)
+
+
+            elif opcion == 3:
+                nombre_actual = input("Nombre del producto que quieres actualizar: ")
+                nuevo_nombre = input("Nuevo nombre: ")
+                nuevo_precio = float(input("Nuevo precio: "))
+                nueva_cantidad = int(input("Nueva cantidad: "))
+                nueva_categoria = input("Nueva categoría: ")
+                self.inventario.actualizar_producto(nuevo_nombre,nueva_cantidad,nueva_categoria,nombre_actual)
+
+
+            elif opcion==4:
+                nombre=input("Nombre del producto a eliminar: ")
+                self.inventario.borrar_producto(nombre)
+
+            #salimos del menú
+            elif opcion==5:
+                break
+    
+if __name__ =="__main__": #Si inventario.py se está ejecutando directamente (no importado en otro archivo), entonces haz esto..
+    conexion=Conexion()
+    inventario=Inventario(conexion)
+    menu=Menu(inventario)
+    menu.mostrar_menu()
+
+              
+
+
+
+            
+        
+
          
              
 
-#Creamos el objeto conexión
-conexion=Conexion()
-#Obtenemos la conexión del pool
-conn=conexion.obtener_conexion()
 
-#A partir de aquí ya podemos trabajar con el cursor y consultas sql
-cursor=conn.cursor()
-cursor.execute("SELECT * FROM productos")
-resultados=cursor.fetchall()
-for productos in resultados:
-     print(productos)
 
 
 
