da9d9c9f79066cd147ad9fa1cf8c2ea5fcae368e
Peter Switch <202387707+peter-switch@users.noreply.github.com>
2025-04-26 10:29:56 +0000

    polling

--- inventario.py
+++ inventario.py
@@ -5,25 +5,44 @@
 # conexión a bases de datos con la biblioteca mysql-connector-python, validación de entradas y manejo de excepciones.
 # Además, se espera que implementes una interfaz de usuario sencilla y funcional que permita realizar las operaciones con fluidez y control de errores.
 
+import mysql.connector
+from mysql.connector import pooling, Error
+#Importamos bibliotecas para trabajar con mysql, el módulo para hacer pooling y otro para trabajar los errores
 
 class Conexion:
 
     def __init__(self):
         #Valores necesarios para conectar a la base de datos
-        host=""
-        database="u762720325_pruebas_python"
-        user="u762720325_admin"
-        password=""
+        self.host=""
+        self.database="u762720325_pruebas_python"
+        self.user="u762720325_admin"
+        self.password=""
 
         #Estos 2 valores son para crear el pool de conexiones
-        pool_name="pool"
-        pool_size=5
+        self.pool_name="pool"
+        self.pool_size=5
 
         #Vamos a crear el pool de conexiones gestionando los posibles errores con try except
         try:
+            #para crear el pool es necesario usar la siguiente instrucción:
+            pool=pooling.MySQLConnectionPool(
 
-        except:
+                host=self.host,
+                database=self.database,
+                user=self.user,
+                password=self.password,
+                pool_name=self.pool_name,
+                pool_size=pool_size
+
+            )
+
+            print="¡Pool de conexiones cerado con exito!"
+
+
+        except Error as e:
+
+            print(f"Error al crear el pool: {e}")
 
     #Creamos le método para conectar a traves del pool
-     def crear_conexion:
+    def crear_conexion(self):
         pass
